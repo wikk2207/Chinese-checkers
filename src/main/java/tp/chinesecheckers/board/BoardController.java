f487adb745e76be17290558ccb7740319280567d
Wiktoria <wiktoriajuliasalamon@gmail.com>
2018-12-04 10:54:22 +0100

    Rozpoczęte rysowanie planszy

---
+++ src/main/java/tp/chinesecheckers/board/BoardController.java
@@ -0,0 +1,13 @@
+package board;
+
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class BoardController extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+    BorderPane borderPane = findViewbyId
+  }
+}
