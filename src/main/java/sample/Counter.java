00a178ac7d3fc9210604c9ddf485816eeb50fa9c
Wiktoria <wiktoriajuliasalamon@gmail.com>
2018-12-28 20:08:06 +0100

    Dodanie i uzupełnienie metod wywoływanych przez klienta do klasy Counter.

--- src/main/java/sample/Counter.java
+++ src/main/java/sample/Counter.java
@@ -9,29 +9,21 @@ import player.RealPlayer;
 public class Counter {
   Player player;
   VBox pane;
+  Client client;
+  boolean correctMove;
+  int id;
+  boolean amIFirst;
 
   Counter() {
-      player = new RealPlayer(this);
+    player=new RealPlayer(this);
+    client=new Client(this);
+    correctMove=false;
 
     setPlayerId(1);
     setBoardSize(17);
     createBoard();
   }
 
-  /**
-   *
-   */
-  public void yourTurn() {
-    player.setMyTurn(true);
-  }
-
-  public void turnEnd() {
-    player.setMyTurn(false);
-  }
-
-  public void uploadMove(int playerId, int fromX, int fromY, int toX, int toY) {
-    player.uploadMove(playerId, fromX, fromY, toX, toY);
-  }
 
   public void setPlayerId(int id) {
     player.setId(id);
@@ -53,26 +45,47 @@ public class Counter {
     return player.getScene();
   }
 
-  public boolean isMoveValid (int fromX, int fromY, int toX, int toY) {
-    //TODO pytaj serwera
-    return true;
+  public boolean isMoveValid(int fromX, int fromY, int toX, int toY) {
+    //TODO ta metoda powinna zmieniać parametr correctMove
+    client.move(fromX, fromY, toX, toY);
+    return correctMove;
   }
 
   public boolean amIFirst() {
-    return true;
+    return amIFirst;
   }
 
   public int getMyId() {
-    //asking serwer
-    return 1;
+    return id;
   }
 
-  public void startGame() {
-    pane.getChildren().setAll(Main.getCounter().getScene().getRoot());
-  }
 
+  //METHODS USING CLIENT
+
+  /**
+   * Method used by
+   *
+   * @param players
+   * @param robots
+   * @param pane
+   */
   public void createGame(int players, int robots, VBox pane) {
-    //TODO poprawić na komunikację z serwerem
+    client.setNumOfPlayers(players, robots);
+    this.pane=pane;
+  }
+
+
+  //METHODS USED BY CLIENT
+
+  public void setId(int id) {
+    this.id=id;
+  }
+
+  public void set_First(boolean isFirst) {
+    this.amIFirst=isFirst;
+  }
+
+  public void setNumberOfPlayers(int players) {
     switch (players) {
       case 2:
         player.addPlayer(4);
@@ -94,8 +107,37 @@ public class Counter {
         player.addPlayer(4);
         break;
     }
+  }
 
-    this.pane = pane;
-    //send players and robots to server
+  public void uploadMove(int playerId, int fromX, int fromY, int toX, int toY) {
+    player.uploadMove(playerId, fromX, fromY, toX, toY);
+  }
+
+  /**
+   *
+   */
+  public void yourTurn() {
+    player.setMyTurn(true);
+  }
+
+  public void turnEnd() {
+    player.setMyTurn(false);
+  }
+
+  public void startGame() {
+    pane.getChildren().setAll(Main.getCounter().getScene().getRoot());
+  }
+
+  public void wrongMove() {
+    correctMove=false;
+  }
+
+  public void correctMove() {
+    correctMove=true;
   }
 }
+
+
+
+
+
