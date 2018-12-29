c79bc1a8e7324858379ede2189ddfe23fb7781e8
matik98 <matik98@users.noreply.github.com>
2018-12-29 20:45:36 +0100

    Przerobienie metody move na boolowską

--- src/main/java/sample/Client.java
+++ src/main/java/sample/Client.java
@@ -137,9 +137,26 @@ public class Client extends Thread{
    * @param endX Pierwsza wspolrzedna konca ruchu
    * @param endY Druga wspolrzedna konca ruchu
    */
-  public void move(int begX, int begY, int endX, int endY) {
+  public boolean move(int begX, int begY, int endX, int endY) {
     output.println(MOVED + " " + begX + " " + begY + " " + endX + " " + endY);
     System.out.println(MOVED + " " + begX + " " + begY + " " + endX + " " + endY);
+    String response;
+    try {
+      while (true) {
+        response = input.readLine();
+        if (response.equals(WRONG_MOVE)) {
+          System.out.println(response);
+          return false;
+        } else if (response.equals(CORRECT_MOVE)) {
+          System.out.println(response);
+          return true;
+        }
+      }
+    } catch (IOException e) {
+      System.err.println("Lost conection with server");
+      System.exit(1);
+    }
+    return false;
   }
 
   /**
